b073957b4c700ba9a0412ceabff0c1a6456ce36a
Masatomo Ide <110540755+HoshinoHanaMizuki@users.noreply.github.com>
2023-12-14 22:04:27 +0900

    Add files via upload

---
+++ scripts/common.js
@@ -0,0 +1,12 @@
+const closeButton=document.querySelector('.closeButton');
+const mobileMenu=document.querySelector('.mobile-menu');
+const menuBar=document.querySelector('.menu-bar');
+closeButton.addEventListener("click",()=>{
+    mobileMenu.style.display="none";
+    menuBar.style.display="block";
+});
+
+menuBar.addEventListener("click",()=>{
+    menuBar.style.display="none";
+    mobileMenu.style.display="block";
+});
