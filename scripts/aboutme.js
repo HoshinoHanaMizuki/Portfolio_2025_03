756c4de44ebd929a70167c9d392b3d606e60d5b7
Masatomo Ide <110540755+HoshinoHanaMizuki@users.noreply.github.com>
2023-12-22 20:00:25 +0900

    Add files via upload

--- scripts/aboutme.js
+++ scripts/aboutme.js
@@ -22,25 +22,20 @@ else{
 
 
 // クリックでz-indexの値をprevで-1,nextで+1
-prevArrow.addEventListener("click",()=>{
-  for(let i=0; i<4; i++){
-    console.log(i+1+"枚目");
+function changeSlide(changeNum,slides){
+  for(let i=0; i<slides.length; i++){
+    console.log(i+1+"枚目だよ");
     console.log(window.getComputedStyle(slides[i]).zIndex);
-    slides[i].style.zIndex = (parseInt(window.getComputedStyle(slides[i]).zIndex) + slides.length - 1) % slides.length;
+    slides[i].style.zIndex = (parseInt(window.getComputedStyle(slides[i]).zIndex) + slides.length + changeNum) % slides.length;
     if(slides[i].style.zIndex==0){
       slides[i].style.zIndex=slides.length;
     }
     console.log(slides[i].style.zIndex);
   }
+}
+prevArrow.addEventListener("click",()=>{
+  changeSlide(-1,slides);
 });
 nextArrow.addEventListener("click",()=>{
-  for(let i=0; i<4; i++){
-    console.log(i+1+"枚目");
-    console.log(window.getComputedStyle(slides[i]).zIndex);
-    slides[i].style.zIndex = (parseInt(window.getComputedStyle(slides[i]).zIndex) + 1) % slides.length;
-    if(slides[i].style.zIndex==0){
-      slides[i].style.zIndex=4;
-    }
-    console.log(slides[i].style.zIndex);
-  }
+  changeSlide(1,slides);
 });
