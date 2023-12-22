32092a6209e1810824bbd184a986cf8cbd862b62
Masatomo Ide <110540755+HoshinoHanaMizuki@users.noreply.github.com>
2023-12-22 21:55:23 +0900

    Add files via upload

--- scripts/aboutme.js
+++ scripts/aboutme.js
@@ -6,20 +6,22 @@ console.log(slides);
 console.log(slides[0].src);
 console.log(slides[0].width);
 
-//画像は、PCサイズのときをデフォルトにしてタブレットサイズ以下のときはそれ用にパスを変更
-if(slides[0].width <= 600 || window.matchMedia("(orientation: portrait)").matches){
-  slides[0].src="../images/aboutme_1_tabsm.png";
-  slides[1].src="../images/aboutme_2_tabsm.png";
-  slides[2].src="../images/aboutme_3_tabsm.png";
-  slides[3].src="../images/aboutme_4_tabsm.png";
+function changeImage(){
+  // デバイスの向きが変更されたときに実行したい処理を記述する
+  if (window.matchMedia("(orientation: landscape)").matches) {
+    // 横向きの場合の処理
+    slides[0].src="../images/aboutme_1_tabsm.png";
+    slides[1].src="../images/aboutme_2_tabsm.png";
+    slides[2].src="../images/aboutme_3_tabsm.png";
+    slides[3].src="../images/aboutme_4_tabsm.png";
+  } else if (window.matchMedia("(orientation: portrait)").matches) {
+    // 縦向きの場合の処理
+    slides[0].src="../images/aboutme_1_PC.png";
+    slides[1].src="../images/aboutme_2_PC.png";
+    slides[2].src="../images/aboutme_3_PC.png";
+    slides[3].src="../images/aboutme_4_PC.png";
+  }
 }
-else{
-  slides[0].src="../images/aboutme_1_PC.png";
-  slides[1].src="../images/aboutme_2_PC.png";
-  slides[2].src="../images/aboutme_3_PC.png";
-  slides[3].src="../images/aboutme_4_PC.png";
-}
-
 
 // クリックでz-indexの値をprevで-1,nextで+1
 function changeSlide(changeNum,slides){
@@ -33,6 +35,11 @@ function changeSlide(changeNum,slides){
     console.log(slides[i].style.zIndex);
   }
 }
+
+//画像は、PCサイズのときをデフォルトにしてタブレットサイズ以下と画面切り替え時のときはそれ用にパスを変更
+window.addEventListener("orientationchange", () => {
+  changeImage();
+});
 prevArrow.addEventListener("click",()=>{
   changeSlide(-1,slides);
 });
