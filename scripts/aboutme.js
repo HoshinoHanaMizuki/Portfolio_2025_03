536d64821d3dad66f07264638e1f693860884b6a
Masatomo Ide <110540755+HoshinoHanaMizuki@users.noreply.github.com>
2023-12-22 19:52:13 +0900

    Add files via upload

--- scripts/aboutme.js
+++ scripts/aboutme.js
@@ -19,3 +19,28 @@ else{
   slides[2].src="../images/aboutme_3_PC.png";
   slides[3].src="../images/aboutme_4_PC.png";
 }
+
+
+// クリックでz-indexの値をprevで-1,nextで+1
+prevArrow.addEventListener("click",()=>{
+  for(let i=0; i<4; i++){
+    console.log(i+1+"枚目");
+    console.log(window.getComputedStyle(slides[i]).zIndex);
+    slides[i].style.zIndex = (parseInt(window.getComputedStyle(slides[i]).zIndex) + slides.length - 1) % slides.length;
+    if(slides[i].style.zIndex==0){
+      slides[i].style.zIndex=slides.length;
+    }
+    console.log(slides[i].style.zIndex);
+  }
+});
+nextArrow.addEventListener("click",()=>{
+  for(let i=0; i<4; i++){
+    console.log(i+1+"枚目");
+    console.log(window.getComputedStyle(slides[i]).zIndex);
+    slides[i].style.zIndex = (parseInt(window.getComputedStyle(slides[i]).zIndex) + 1) % slides.length;
+    if(slides[i].style.zIndex==0){
+      slides[i].style.zIndex=4;
+    }
+    console.log(slides[i].style.zIndex);
+  }
+});
