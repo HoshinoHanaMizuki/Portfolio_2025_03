94ff09a61e32420ec36f229a20968d683bde3afb
Masatomo Ide <110540755+HoshinoHanaMizuki@users.noreply.github.com>
2023-12-22 18:14:45 +0900

    Add files via upload

--- scripts/aboutme.js
+++ scripts/aboutme.js
@@ -1,22 +1,21 @@
-const swiper = new Swiper('.introSlider', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    effect:'coverflow',
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const slides=document.querySelectorAll(".slide");
+const prevArrow=document.querySelector(".prevArrow");
+const nextArrow=document.querySelector(".nextArrow");
+
+console.log(slides);
+console.log(slides[0].src);
+console.log(slides[0].width);
+
+//画像は、PCサイズのときをデフォルトにしてタブレットサイズ以下のときはそれ用にパスを変更
+if(slides[0].width <= 600 || window.matchMedia("(orientation: portrait)").matches){
+  slides[0].src="../images/aboutme_1_tabsm.png";
+  slides[1].src="../images/aboutme_2_tabsm.png";
+  slides[2].src="../images/aboutme_3_tabsm.png";
+  slides[3].src="../images/aboutme_4_tabsm.png";
+}
+else{
+  slides[0].src="../images/aboutme_1_PC.png";
+  slides[1].src="../images/aboutme_2_PC.png";
+  slides[2].src="../images/aboutme_3_PC.png";
+  slides[3].src="../images/aboutme_4_PC.png";
+}
