683aa2145716d0e247dd219a7561b5c66d7edfcb
Masatomo Ide <110540755+HoshinoHanaMizuki@users.noreply.github.com>
2023-12-23 15:15:17 +0900

    Add files via upload

--- scripts/common.js
+++ scripts/common.js
@@ -1,15 +1,24 @@
 const closeButton=document.querySelector('.closeButton');
 const mobileMenu=document.querySelector('.mobile-menu');
 const menuBar=document.querySelector('.menu-bar');
+const header=document.querySelector("#header");
+const mainContainer=document.querySelector("#mainContainer");
+const footer=document.querySelector("#footer");
 
 closeButton.addEventListener("click",()=>{
     mobileMenu.style.display="none";
     menuBar.style.display="block";
+    header.style.opacity='1.0';
+    mainContainer.style.opacity='1.0';
+    footer.style.opacity='1.0';
 });
 
 menuBar.addEventListener("click",()=>{
     menuBar.style.display="none";
     mobileMenu.style.display="block";
+    header.style.opacity='0.5';
+    mainContainer.style.opacity='0.5';
+    footer.style.opacity='0.5';
 });
 
 // スクロール監視用コールバック関数とクラス生成
